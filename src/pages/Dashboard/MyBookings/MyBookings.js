d1b0dd602cff48b438f1faec8ea7b3b7999a3232
RHN007 <rakibul.nayon@outlook.com>
2022-11-28 22:26:17 +0600

    adding few

--- src/pages/Dashboard/MyBookings/MyBookings.js
+++ src/pages/Dashboard/MyBookings/MyBookings.js
@@ -1,14 +1,20 @@
 import { useQuery } from '@tanstack/react-query';
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import { AuthContext } from '../../../contexts/AuthProvider';
+import ConfirmationModal from '../../Shared/ConfirmationModal/ConfirmationModal';
 
 const MyBookings = () => {
     const {user} = useContext(AuthContext)
+    const [deletedWish, setDeletedWish] = useState(null)
+    const closeModal = () => {
+        setDeletedWish(null)
+    }
     const url = `https://server-sooty-five.vercel.app/bookings?email=${user?.email}`; 
     // const url = `https://server-sooty-five.vercel.app/bookings`; 
 
-    const {data:bookings= []} = useQuery({
+    const {data:bookings= [], refetch} = useQuery({
         queryKey: ['bookings', user?.email], 
         queryFn: async() => {
             const res = await fetch(url, {
@@ -21,11 +27,32 @@ const MyBookings = () => {
 
         }
     })
+    const handleDeleteWishlist = ad => {
+        console.log(ad)
+        fetch(`https://server-sooty-five.vercel.app/bookings/${ad._id}`, {
+            method: 'DELETE',
+            headers: {
+                authorization: `bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+        .then(res => res.json())
+        .then(data => {
+            if(data.deletedCount>0){
+                refetch()
+                toast.success(`Advertisement ${ad.name} deleted Successfully`)
+            }
+            else {
+                toast.error( 'Sorry You cannot delete'
+                )
+            }
+        })
+    }
+
 
 
     return (
         <div>
-        <h3 className="text-3xl mb-5">My Appointments</h3>
+        <h3 className="text-3xl mb-5">My Bookings</h3>
         <div className="overflow-x-auto">
             <table className="table w-full">
                 <thead>
@@ -35,6 +62,7 @@ const MyBookings = () => {
                         <th>Name</th>
                         <th>Brand</th>
                         <th>Price</th>
+                        <th>Action</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -61,11 +89,25 @@ const MyBookings = () => {
                                     booking.price && booking.paid && <span className='text-green-500 '>Paid</span>
                                 }
                             </td>
+                            <td>
+                                <label onClick={() =>setDeletedWish(booking)} htmlFor="confirmation-modal" className="btn btn-sm btn-error">Delete</label>
+                            </td>
                         </tr>)
                     }
                 </tbody>
             </table>
         </div>
+        {
+            deletedWish && <ConfirmationModal
+                title={`Are you sure you want to delete?`}
+                message={`If you delete ${deletedWish.name}. It cannot be undone.`}
+                successAction = {handleDeleteWishlist}
+                successButtonName="Delete"
+                modalData = {deletedWish}
+                closeModal = {closeModal}
+            >
+            </ConfirmationModal>
+        }
     </div>
     );
 };
